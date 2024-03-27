b1393ac701841b20299956a5565ef18bb079e9b6
yumdmb <elvissawing.muran@gmail.com>
2024-03-27 23:53:30 +0800

    center and style searchbox

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { robots } from "./robots";
 
 const App = () => {
   return (
-    <div>
+    <div className="tc">
       <h1>RoboFriends</h1>
       <SearchBox />
       <CardList robots={robots} />;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const SearchBox = () => {
-    return (
-        <input type='search' placeholder='search robots' />
-    );
-}
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
