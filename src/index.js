1f475fc2a7ab95c156535f4b05f10f616fd87903
yumdmb <elvissawing.muran@gmail.com>
2024-03-27 23:50:04 +0800

    add searchbox

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 //import App from './App';
-import CardList from "./CardList";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import { robots } from "./robots";
+import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>
 );
 
