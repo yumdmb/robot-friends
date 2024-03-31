6a24f148ceec168b0c5c6388141f3e8e85f9d2f2
yumdmb <elvissawing.muran@gmail.com>
2024-04-01 00:37:35 +0800

    fetch robots data using api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+//import { robots } from "./robots"; // we dont use this anymore because we are fetching from an API
 import "./App.css";
 
 //STATE object that describes your application
@@ -9,42 +9,55 @@ import "./App.css";
 //PROPS just read only, never change
 //STATE >> PROPS
 
-
 //use constructor to create state
 class App extends Component {
   constructor() {
-    super()
-    this.state= {
-      robots: robots,
-      searchfield: '',
-    }
+    super();
+    this.state = {
+      //robots: robots, //robots : [] will be empty array
+      robots: [],
+      searchfield: "",
+    };
   }
 
-onSearchChange = (event) =>{
-  this.setState({searchfield : event.target.value})
-  // const filteredRobots = this.state.robots.filter(robots => {
-  //   return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-  // })
-  // console.log(filteredRobots);
-} 
-//anytime input is changed, it will log the event
-// console.log(event.target.value); will log the value of the input
-//return robots.name.toLowerCase().includes(searchfield.toLowerCase()) will return the robots that match the input
-//includes(searchfield.toLowerCase()) will return the robots that match the input
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+      //.then((users) => {}); //this is return empty array
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+    // const filteredRobots = this.state.robots.filter(robots => {
+    //   return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    // })
+    // console.log(filteredRobots);
+  };
+  //anytime input is changed, it will log the event
+  // console.log(event.target.value); will log the value of the input
+  //return robots.name.toLowerCase().includes(searchfield.toLowerCase()) will return the robots that match the input
+  //includes(searchfield.toLowerCase()) will return the robots that match the input
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
-    return (
-      <div className="tc">
-        <h1 className="f2">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />;
-      </div>
-    );
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />;
+        </div>
+      );
+    }
   }
 }
-//access robots using this.state.robots 
+//access robots using this.state.robots
 //state is passed down to components as props
 //CardList accepts robots as props even in App.js it is state
 export default App;
